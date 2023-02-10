b8f7fc7c393fc90b946648712dd06cb3fb0fd2d7
Spencer <craigs1471@gmail.com>
2023-02-10 23:48:22 +0000

    Initial commit

---
+++ src/main/java/com/gssprototype/GssPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.gssprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GssPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GssPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gssprototype/GssPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gssprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GssPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
